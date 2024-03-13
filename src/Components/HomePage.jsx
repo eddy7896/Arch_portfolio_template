154f969f6f46ff463950b5308c49d035fcedfc7c
Amparo Cabezuelo Sepulveda <amcasep@gmail.com>
2024-03-13 18:04:12 +0100

    updated carousel with 10 pictures

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -3,18 +3,24 @@ import img2 from "../images/img2.jpg"
 import img3 from "../images/img3.jpg"
 import img4 from "../images/img4.jpg"
 import img5 from "../images/img5.jpg"
+import img6 from "../images/img6.jpg"
+import img7 from "../images/img7.jpg"
+import img8 from "../images/img8.jpg"
+import img9 from "../images/img9.jpg"
+import img10 from "../images/img10.jpg"
+
 
 import { useEffect, useState } from "react"
 
 const HomePage = () => {
         const [currentSlide, setCurrentSlide] = useState(0);
-        const images = [img1, img2, img3, img4, img5];
+        const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
 
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentSlide((prev) => (prev + 1) % images.length);
             console.log(currentSlide);
-        },5000);
+        },4000);
 
         return () => clearInterval(interval);
     }, [images.length]); 
